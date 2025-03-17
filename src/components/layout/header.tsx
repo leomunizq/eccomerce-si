daee876fa05c40cbfd6840ba36e12ea2b6933717
Leonardo Muniz <leonardo.mdq@hotmail.com>
2025-03-17 11:51:38 +0100

    add padding to header container for improved layout

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <header className="bg-gray-900 text-white py-4">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center px-4">
         <Link to="/" className="text-2xl font-bold">Lorem Ipsum Store</Link>
         <nav className="space-x-4">
           <Link to="/" className="hover:underline">Home</Link>
