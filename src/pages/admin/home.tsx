cc38a2c85dd6e2e2f605acb14958522b7eec7ca2
Leonardo Muniz <leonardo.mdq@hotmail.com>
2025-03-17 11:18:45 +0100

    fetch and display total counts for products, brands, and categories on admin dashboard

--- src/pages/admin/home.tsx
+++ src/pages/admin/home.tsx
@@ -1,25 +1,50 @@
-import { Box, Package, Tag, Users } from "lucide-react"
-
+import { useBrands } from '@/hooks/brands/use-brands'
+import { useCategories } from '@/hooks/categories/use-categories'
+import { useProducts } from '@/hooks/products/use-products'
+import { Box, Package, Tag } from 'lucide-react'
 
 export const AdminPage = () => {
- 
+  const { data: productsData } = useProducts()
+  const { data: brandsData } = useBrands()
+  const { data: categoriesData } = useCategories()
+
+  const totalProducts = productsData?.total
+  const totalBrands = brandsData?.length
+  const totalCategories = categoriesData?.length
+
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 mt-14">
-      <DashboardCard title="Total Products" value="248" icon={<Package className="h-4 w-4 text-muted-foreground" />} />
-      <DashboardCard title="Total Brands" value="36" icon={<Tag className="h-4 w-4 text-muted-foreground" />} />
-      <DashboardCard title="Total Categories" value="12" icon={<Box className="h-4 w-4 text-muted-foreground" />} />
-      <DashboardCard title="Total Customers" value="1,024" icon={<Users className="h-4 w-4 text-muted-foreground" />} />
+      {totalProducts && (
+        <DashboardCard
+          title="Total Products"
+          value={totalProducts?.toString()}
+          icon={<Package className="h-4 w-4 text-muted-foreground" />}
+        />
+      )}
+      {totalBrands && (
+        <DashboardCard
+          title="Total Brands"
+          value={totalBrands.toString()}
+          icon={<Tag className="h-4 w-4 text-muted-foreground" />}
+        />
+      )}
+      {totalCategories && (
+        <DashboardCard
+          title="Total Categories"
+          value={totalCategories.toString()}
+          icon={<Box className="h-4 w-4 text-muted-foreground" />}
+        />
+      )}
     </div>
   )
 }
 
 export default AdminPage
 
-
 function DashboardCard({
   title,
   value,
-  icon,
+  icon
 }: {
   title: string
   value: string
